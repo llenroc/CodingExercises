7d61152f9d878d273a8590b069307b1b1e2f903d
Domingo Perez <djperezh@users.noreply.github.com>
2020-07-10 13:16:44 -0700

    Include Test project

---
+++ C-Sharp/Exercises.Tests/InterviewCakeTests/ChapterOne_ArraysTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Exercises;
+
+namespace Exercises.Tests
+{
+    public class ChapterOne_ArraysTests
+    {
+        [Fact]
+        public void MyMethodTests()
+        {
+            var result = ChapterOne_Arrays.MyMethod();
+            Assert.False(result);
+        }
+    }
+}
---
+++ C-Sharp/Exercises/InterviewCake/ChapterOne_Arrays.cs
@@ -0,0 +1,11 @@
+namespace Exercises
+{
+    public static class ChapterOne_Arrays
+    {
+        public static bool MyMethod()
+        {
+            return false;
+        }
+    }
+
+}
